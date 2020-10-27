49da359a15f17d6ac7619e494eddd8f3847195f3
Carly Pecora <carly@carlypecora.com>
2020-10-26 21:46:59 -0400

    frontend talks to backend

---
+++ client/src/Main.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+
+const Main = () => {
+	const [response, setResponse] = useState('')
+	const [post, setPost] = useState('')
+	const [responseToPost, setResponseToPost] = useState('')
+
+	useEffect(() => {
+		callApi()
+		.then(res => setResponse(res.express))
+      	.catch(err => console.log(err))
+	}, [])
+
+	const callApi = async () => {
+	    const response = await fetch('/api/hello');
+	    const body = await response.json();
+	    if (response.status !== 200) throw Error(body.message);
+	    
+	    return body;
+	  };
+
+	const handleSubmit = async e => {
+	    e.preventDefault();
+	    const response = await fetch('/api/world', {
+	      method: 'POST',
+	      headers: {
+	        'Content-Type': 'application/json',
+	      },
+	      body: JSON.stringify({ post }),
+	    });
+	    const body = await response.text();
+	    
+	    setResponseToPost(body);
+	  };
+
+	 return (
+	 	<div>
+			<p>{response}</p>
+		        <form onSubmit={handleSubmit}>
+		          <p>
+		            <strong>Post to Server:</strong>
+		          </p>
+		          <input
+		            type="text"
+		            value={post}
+		            onChange={e => setPost(e.target.value)}
+		          />
+		          <button type="submit">Submit</button>
+		        </form>
+	        <p>{responseToPost}</p>
+	 	</div>
+	 )
+}
+
+export default Main
